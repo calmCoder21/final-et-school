8c138854edeada9eaf700f14dedc3658d2715332
calmCoder21 <natnaelkassahun13@gmail.com>
2026-04-25 18:53:34 +0300

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -70,7 +70,7 @@ export default function SignupPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: "https://final-et-school.vercel.app/auth/callback",
       },
     });
 
